8ae58a68891a0782256ec619f70cf43c71cc0f79
Or Sason <ors2342@gmail.com>
2021-03-27 04:49:45 -0400

    ps_update

--- Powershell/taskManager.ps1
+++ Powershell/taskManager.ps1
@@ -1,13 +1,51 @@
 
-function Create-Task {
+function Create-Task{
     [CmdletBinding()]
     param (
-        $action,$trigger 
+        $taskName,$time
     )
     process {
-    $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'c:/myTask.ps1' 
-    $trigger = New-ScheduledTaskTrigger  -daily -at 21:00
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "myTask" -Description "Run task every 30 seconds"
+    $action = New-ScheduledTaskAction   -Argument 'c:/test/myTask.ps1'  -Execute 'powershell.exe'
+    $trigger = New-ScheduledTaskTrigger -Once -at (Get-Date) -RepetitionInterval (New-TimeSpan -Seconds $time) 
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName  -Description "Run and repeat task from input"
+    Start-ScheduledTask myTask
     }
-    
+
 }
+
+
+function Get-AllTasks {
+    [CmdletBinding()]
+    param (  )
+    process {
+    Get-ScheduledTask |Select-Object TaskName|Sort-Object
+    }
+}
+
+
+function Change-TaskStatus {
+    [CmdletBinding()]
+    param ( $taskName)
+    process {
+       
+        try{
+
+        $task = Get-ScheduledTask $taskName
+        if($task.State -eq 'Ready')
+        {
+            Stop-ScheduledTask -TaskName $taskName
+            Disable-ScheduledTask -TaskName $taskName
+        }
+        elseif($task.State -eq 'Disable')
+        {
+        Enable-ScheduledTask -TaskName $taskName
+        Start-ScheduledTask -TaskName $taskName  
+        }
+    }
+    catch{
+        Read-Host 'Could not found Proccess' 
+    }
+    }
+}
+
+

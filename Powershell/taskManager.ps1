ce0d68f3bea92e68215af5b55e4f6c486d1ebab7
Legans09 <72452018+Legans09@users.noreply.github.com>
2021-03-29 22:36:16 +0300

    PS

--- Powershell/taskManager.ps1
+++ Powershell/taskManager.ps1
@@ -4,6 +4,8 @@
 param 
 ([Parameter(Position=0)][string]$TaskName, [Parameter(Position=1)][int]$WaitInSeconds)
 
+Write-Output "For more information type 'help'"
+
 function Create-Task{
     [CmdletBinding()]
     param (
@@ -12,7 +14,7 @@ function Create-Task{
     process {
     $action = New-ScheduledTaskAction   -Argument 'c:/test/myTask.ps1'  -Execute 'powershell.exe'
     $trigger = New-ScheduledTaskTrigger -Once -at (Get-Date) -RepetitionInterval (New-TimeSpan -Seconds $WaitInSeconds) 
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName  -Description "Run and repeat task from input"
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $TaskName  -Description "Run and repeat task from input" 
     Start-ScheduledTask $TaskName
     }
 
@@ -55,8 +57,26 @@ function Change-TaskStatus {
     }
 }
 
+# print information about the functions and Examples
+function Help {
+    [CmdletBinding()]
+    param (  )
+    process {
+    Write-Output "Create-Task [TaskName] [waitInSeconds]  - Creating a task that would get as input name and WaitInSeconds"
+    Write-Output "Get-AllTasks - Print all running tasks"
+    Write-Output "Change-TaskStatus [TaskName] - Enable/Disable a task depends on task's status"
+    }
+}
+
 #if the user entered both task's name and wait in Seconds lunch Create-task function
-if ( $null -ne  $TaskName && $null -ne $WaitInSeconds)
+
+try{
+if ( $null -ne  $TaskName -and $null -ne $WaitInSeconds)
 {
     Create-Task $TaskName $WaitInSeconds
+}
+}
+catch{
+
+Write-Output "No variables entered"
 }
